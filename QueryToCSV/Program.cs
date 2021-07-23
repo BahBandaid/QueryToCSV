0433816fa3c880a1263d5c9c0458d8badfd8744c
bahbahbahh <59581586+BahBandaid@users.noreply.github.com>
2021-07-23 13:17:53 -0700

    Add project files.

---
+++ QueryToCSV/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QueryToCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
